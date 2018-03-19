bfb12fcb257600b6924e2861574f884944f5cf2a
slledru <slledru@msn.com>
2018-03-19 14:03:28 -0600

    readdir

--- my-files.js
+++ my-files.js
@@ -1,7 +1,18 @@
 const { red, yellow } = require('chalk')
 const path = require('path')
+const fs = require('fs')
 
-const filename = path.join(__dirname, 'test.txt')
+// const filename = path.join(__dirname, 'test.txt')
+//
+// console.log(red('Hello'));
+// console.log(yellow(filename));
 
-console.log(red('Hello'));
-console.log(yellow(filename));
+fs.readdir(__dirname, (err, files) => {
+  if (err) {
+    console.log(red(err));
+  } else {
+    console.log(files);
+  }
+})
+
+console.log(yellow('Hello!'));
