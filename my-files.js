e92155feff93cc63b096cf91ab6b9d4f55552a8e
slledru <slledru@msn.com>
2018-03-19 14:57:42 -0600

    read/write

--- my-files.js
+++ my-files.js
@@ -37,10 +37,45 @@ const filename = path.join(__dirname, 'test.txt')
 //   console.log(red(err));
 // }
 
-fs.unlink(filename, (err) => {
-  if (err) {
-    console.log(red(err))
-    return
-  }
-  console.log('file has been deleted');
-})
+// fs.unlink(filename, (err) => {
+//   if (err) {
+//     console.log(red(err))
+//     return
+//   }
+//   console.log('file has been deleted');
+// })
+
+function addFruit(type, count, filename='things.csv') {
+  fs.readFile(filename, 'utf8', (error, data) => {
+    if (error) {
+      console.log(red(error))
+      return
+    }
+    const lines = data
+      .split('\n')
+      .filter((line) => line.length > 0)
+      .map((line, index) => line.split(','))
+      .reduce((last, line) => {
+        last[line[0]] = line[1]
+        return last
+      }, {})
+
+    lines[type] = count
+    console.log(lines);
+
+    const newLines = []
+    Object.keys(lines).forEach(key => {
+      newLines.push(`${key},${lines[key]}`)
+    })
+    const writeMe =  newLines.join('\n')
+    fs.writeFile(filename, writeMe, { encoding : 'utf8', flag : 'w' }, (err) => {
+      if (err) {
+        console.log(red(err))
+        return
+      }
+      console.log('file has been saved');
+    })
+  })
+}
+
+addFruit('Apples', 111)
